28d03ed3274d18531731d93c4c985f652f94074a
keptupp <2315584373@qq.com>
2024-07-03 14:07:26 +0800

    s

--- babyllama/chat_interface.py
+++ babyllama/chat_interface.py
@@ -58,7 +58,7 @@ def chat_epoch(model,dict_data):
 
         token=torch.tensor(reference_token+total_token, dtype=torch.long, device="cuda").unsqueeze(0)
 
-        pre_tokens=model.inference(token,prev_pos=0,max_length=256,top_p=0.9)
+        pre_tokens=model.inference(token,prev_pos=0,max_length=256,top_p=0.5)
 
 
         pre_text_list=[model.tokenizer.decode(pre_tokens[i]) for i in range(len(pre_tokens))]
@@ -92,7 +92,7 @@ if __name__=="__main__":
         max_batch_size=8,
     ).to(config.device)
 
-    model.load_state_dict(torch.load("weight/pre_train/epoch_3.pt"))
+    model.load_state_dict(torch.load("weight/belle_epoch_3.pt"))
 
     dict_data=dict()
 
